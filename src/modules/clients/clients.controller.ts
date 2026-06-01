a7328cb39c8061e83543a66932b83d347cfeb9b5
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-06-01 18:04:05 +0200

    client services

--- src/modules/clients/clients.controller.ts
+++ src/modules/clients/clients.controller.ts
@@ -37,7 +37,6 @@ export class ClientsController {
   @ApiParam({ name: 'id', description: 'Client ID (CUID).' })
   update(@Param('id') id: string, @Body() body: Partial<CreateClientDto>) {
     return this.clientsService.update(id, body);
-    
   }
 }
 
