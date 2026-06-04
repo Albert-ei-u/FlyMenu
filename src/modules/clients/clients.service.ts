1a78a9965969ef4fbcc4de4b16c0c9a1ad00bd70
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-06-04 15:04:05 +0200

    client services

--- src/modules/clients/clients.service.ts
+++ src/modules/clients/clients.service.ts
@@ -17,11 +17,12 @@ export class ClientsService {
   async findAll(user?: CurrentUser) {
     const restaurantId = user ? await getRestaurantIdForUser(this.prisma, user) : undefined;
     
-    
+
     const where = restaurantId ? {
 
       restaurants: {
         some: {
+          
 
           restaurantId: restaurantId
         }
